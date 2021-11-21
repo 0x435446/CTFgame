bc543d5e1b682edcf9e9c0cbcb76f3513ef0c140
0x435446 <0x435446@gmail.com>
2021-11-21 09:00:32 -0500

    LinkServer

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template,jsonify
 from flask import request,redirect,session,send_file
 
+import MySQLdb 
 app = Flask(__name__)
 
 app.secret_key = 'PapanasiCuBranza123456'
@@ -22,8 +23,17 @@ def index():
 @app.route('/getInfo', methods=['GET', 'POST'])
 def getInfo():
 	if request.method == 'POST':
+		db=MySQLdb.connect(host="localhost",user="root",passwd="FlagFlag123.",db="platformgame" )
 		Data = request.form
-		return Data['Challenge']
+		ID = int(Data['Challenge']) + 1
+		cursor= db.cursor()
+		cursor.execute("SELECT * FROM challenges WHERE ID='"+str(ID)+"'")
+		data = (cursor.fetchall())
+		Name = data[0][1]
+		Descriere = data[0][3]
+		Location = data[0][6]
+		Category = data[0][7]
+		return Name + "|" + Descriere + "|" + Location + "|" + Category
 
 
 
